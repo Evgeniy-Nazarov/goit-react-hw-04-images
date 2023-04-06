0703f5b7c98395df907e65c1f22492ca701e47f2
Evgeniy-Nazarov <91555886+Evgeniy-Nazarov@users.noreply.github.com>
2023-04-05 22:45:33 -0400

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react"
+
+export class App extends Component {
+
+  state = {
+    image: null,
+    loading: false,
+  }
+  
+  componentDidMount() { 
+    fetch('https://pixabay.com/api/?q=cat&page=1&key=33830559-b80d1d0487f9caaadda577109&image_type=photo&orientation=horizontal&per_page=12')
+      .then(res => res.json())
+      .then(image => this.setState({image}))
+  }
+  render() {
+    return (
+      <div>
+        { this.state.image && (<div> My image after fetch</div>)}
+      </div>
+    );
+  };
+}
