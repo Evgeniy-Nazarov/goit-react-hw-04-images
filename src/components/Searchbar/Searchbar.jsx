89bed7bbe138da57b224f478747729fd66a26f8b
Evgeniy-Nazarov <91555886+Evgeniy-Nazarov@users.noreply.github.com>
2023-04-08 22:22:57 -0400

    Update Searchbar.jsx

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,55 @@
+import { Component } from "react";
+  import { toast } from 'react-toastify';
+
+
+
+export default class Searchbar extends Component { 
+
+  state = {
+    tags: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({tags: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.tags.trim() === '') {
+      toast('Введите название картинки' ,{
+position: "top-right",
+autoClose: 5000,
+hideProgressBar: false,
+closeOnClick: true,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "light",
+});
+      return;
+    }
+    this.props.onSubmit(this.state.tags);
+    this.setState({tags: ''});
+  };
+
+    render() {
+        return (
+            <header className="searchbar">
+  <form className="form" onSubmit={this.handleSubmit}>
+    <button type="submit" className="button">
+      <span className="button-label">Search</span>
+    </button>
+
+    <input
+      className="input"
+      type="text"
+      placeholder="Search images and photos"
+      value={this.state.tags}
+      onChange={this.handleNameChange}
+    />
+  </form>
+</header>
+        )
+     }
+
+}
