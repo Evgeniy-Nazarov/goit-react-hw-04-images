2daeca60a42f363e684b653ab0a965e16a42b474
Evgeniy-Nazarov <91555886+Evgeniy-Nazarov@users.noreply.github.com>
2023-04-08 22:23:03 -0400

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,39 @@
 import { Component } from "react"
+  import { ToastContainer} from 'react-toastify';
+
+import Searchbar from "./Searchbar/Searchbar";
 
 export class App extends Component {
 
   state = {
-    image: null,
-    loading: false,
+    tags: '',
   }
+
+  handleSubmit = tags => {
+    this.setState({ tags });
+  };
   
-  componentDidMount() { 
-    fetch('https://pixabay.com/api/?q=cat&page=1&key=33830559-b80d1d0487f9caaadda577109&image_type=photo&orientation=horizontal&per_page=12')
-      .then(res => res.json())
-      .then(image => this.setState({image}))
-  }
+  // componentDidMount() { 
+  //   fetch('https://pixabay.com/api/?q=cat&page=1&key=33830559-b80d1d0487f9caaadda577109&image_type=photo&orientation=horizontal&per_page=12')
+  //     .then(res => res.json())
+  //     .then(image => this.setState({image}))
+  // }
   render() {
     return (
       <div>
-        { this.state.image && (<div> My image after fetch</div>)}
+        <Searchbar onSubmit={this.handleSubmit} />
+        <ToastContainer position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
+
       </div>
     );
   };
